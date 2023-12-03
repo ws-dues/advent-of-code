6090d40fdfbf31ffc236de0a3ea26f200a7ac626
Jacob Walker <ws-dues@users.noreply.github.com>
2023-12-02 22:02:35 -0600

    bring Day1 solution over from work repo

--- 2023/Day1/src/Day1.ts
+++ 2023/Day1/src/Day1.ts
@@ -1,6 +1,97 @@
-const Day1 = () => {
-  console.log('Do something, Day1');
+import { join, parse } from "path";
+import { syncReadFile } from "../../../utils";
+
+export const Day1Puzzle1 = () => {
+  // parse the input.txt file into a string
+  const elfCalibrationText = syncReadFile(join(__dirname, "../input.txt"));
+
+  // elf inventories are separated by blank lines, split into array of strings
+  // representing separate elf calibration strings
+  const elfCalibrationRawValues = elfCalibrationText.split("\n");
+
+  // For each raw value, identify the double-digit value hidden therein
+  // Note that when only a single digit is present, the double-digit value
+  // has that digit in both places.
+  const revealedNumbers = elfCalibrationRawValues.map((fullString) => {
+    const digitsFromString = Array.from(fullString.matchAll(/([0-9]){1}/g));
+    // refactor: this is not readable
+    const revealedValue =
+      digitsFromString[0][0] + digitsFromString[digitsFromString.length - 1][0];
+    return Number.parseInt(revealedValue);
+  });
+
+  return revealedNumbers.reduce((sum, currentNumber) => sum + currentNumber, 0);
 };
 
-export default Day1;
+const parseNumberFromString = (numberString: string): number => {
+  const digit = Number.parseInt(numberString);
+  if (digit) {
+    return digit;
+  }
 
+  switch (numberString) {
+    case "one":
+      return 1;
+    case "two":
+      return 2;
+    case "three":
+      return 3;
+    case "four":
+      return 4;
+    case "five":
+      return 5;
+    case "six":
+      return 6;
+    case "seven":
+      return 7;
+    case "eight":
+      return 8;
+    case "nine":
+      return 9;
+    default:
+      return 0;
+  }
+};
+
+const reverseString = (inputString: string): string => {
+  return Array.from(inputString).reverse().join("");
+};
+
+export const Day1Puzzle2 = () => {
+  // parse the input.txt file into a string
+  const elfCalibrationText = syncReadFile(join(__dirname, "../input.txt"));
+
+  // elf inventories are separated by blank lines, split into array of strings
+  // representing separate elf calibration strings
+  const elfCalibrationRawValues = elfCalibrationText.split("\n");
+
+  // For each raw value, identify the double-digit value hidden therein
+  // Note that when only a single digit is present, the double-digit value
+  // has that digit in both places.
+  const revealedNumbers = elfCalibrationRawValues.map((fullString) => {
+    const firstDigitString = fullString.match(
+      /([0-9]){1}|one|two|three|four|five|six|seven|eight|nine/g
+    )?.[0];
+
+    // refactor: how better to find last digit?
+    // move names into array and use that with find logic instead of regexp?
+    const lastDigitStringReversed = reverseString(fullString).match(
+      /([0-9]){1}|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/g
+    )?.[0];
+
+    if (
+      firstDigitString === undefined ||
+      lastDigitStringReversed === undefined
+    ) {
+      throw new Error(`No digit found in ${fullString}`);
+    }
+
+    const lastDigitString = reverseString(lastDigitStringReversed);
+    return (
+      parseNumberFromString(firstDigitString) * 10 +
+      parseNumberFromString(lastDigitString)
+    );
+  });
+
+  return revealedNumbers.reduce((sum, currentNumber) => sum + currentNumber, 0);
+};
--- 2023/Day1/src/index.ts
+++ 2023/Day1/src/index.ts
@@ -1,4 +1,4 @@
-import Day1 from './Day1';
+import { Day1Puzzle1, Day1Puzzle2 } from './Day1';
 
-Day1();
-
+console.log('Solution to Puzzle 1: ', Day1Puzzle1());
+console.log('Solution to Puzzle 2: ', Day1Puzzle2());
