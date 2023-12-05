e827a81045af537714012deb0c22efe855ddfba2
Jacob Walker <ws-dues@users.noreply.github.com>
2023-12-04 21:34:07 -0600

    solve day 2 puzzle 1 (messy)

--- 2023/Day2/src/index.ts
+++ 2023/Day2/src/index.ts
@@ -1,3 +1,4 @@
-import Day2 from './Day2';
+import { Day2Puzzle1 } from "./Day2";
 
-Day2();
+console.log('Solution to Puzzle 1: ', Day2Puzzle1());
+// console.log('Solution to Puzzle 2: ', Day2Puzzle2());
