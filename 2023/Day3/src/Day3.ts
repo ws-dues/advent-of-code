83a8fc32182c9751efd74bfcd76aaca5fd877c7e
Jacob Walker <ws-dues@users.noreply.github.com>
2023-12-05 23:21:43 -0600

    add template for day 3

---
+++ 2023/Day3/src/Day3.ts
@@ -0,0 +1,8 @@
+export const Day3Puzzle1 = () => {
+  console.log('Do something for puzzle 1, Day3');
+};
+
+export const Day3Puzzle2 = () => {
+  console.log('Do something for puzzle 2, Day3');
+};
+
---
+++ 2023/Day3/src/index.ts
@@ -0,0 +1,5 @@
+import { Day3Puzzle1, Day3Puzzle2 } from './Day3';
+
+console.log('Solution to Puzzle 1: ', Day3Puzzle1());
+console.log('Solution to Puzzle 2: ', Day3Puzzle2());
+
