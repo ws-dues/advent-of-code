053a4b42333371191603f89aecc6dd13ecd2a3af
Jacob Walker <ws-dues@users.noreply.github.com>
2023-12-05 22:36:30 -0600

    solve day 2 puzzle 2 (messy)

--- 2023/Day2/src/Day2.ts
+++ 2023/Day2/src/Day2.ts
@@ -37,7 +37,7 @@ export const Day2Puzzle1 = () => {
       })
     );
 
-    if(isGamePossibleWithinBounds) {
+    if (isGamePossibleWithinBounds) {
       return sum + gameId;
     }
 
@@ -45,4 +45,55 @@ export const Day2Puzzle1 = () => {
   }, 0);
 
   return sumIds;
+};
+
+export const Day2Puzzle2 = () => {
+  // parse the input.txt file into a string
+  const cubeGamesRecordText = syncReadFile(join(__dirname, "../input.txt"));
+
+  // elf inventories are separated by blank lines, split into array of strings
+  // representing separate elf calibration strings
+  const cubeGamesRecordList = cubeGamesRecordText.split("\n");
+
+  const sumPowers = cubeGamesRecordList.reduce((sum, currentCubeGameRecord) => {
+    // split game id from sequence of cube subsets
+    const [_, rawCubeSubsetsString] = currentCubeGameRecord.split(":");
+
+    const rawCubeSubsets = rawCubeSubsetsString
+      .split(";")
+      .map((subset) => subset.split(","));
+
+    // Determine the maximum number of each type of cube for this game record
+    const maxDiceCounts = rawCubeSubsets.reduce(
+      (currentMaxDiceCounts, rawCubeSubset) => {
+        rawCubeSubset.forEach((diceRecord) => {
+          const [diceNumberString, color] = diceRecord.trim().split(" ");
+          const diceNumber = Number.parseInt(diceNumberString);
+
+          currentMaxDiceCounts.red =
+            color === "red" && currentMaxDiceCounts.red < diceNumber
+              ? diceNumber
+              : currentMaxDiceCounts.red;
+
+          currentMaxDiceCounts.green =
+            color === "green" && currentMaxDiceCounts.green < diceNumber
+              ? diceNumber
+              : currentMaxDiceCounts.green;
+
+          currentMaxDiceCounts.blue =
+            color === "blue" && currentMaxDiceCounts.blue < diceNumber
+              ? diceNumber
+              : currentMaxDiceCounts.blue;
+        });
+
+        return currentMaxDiceCounts;
+      },
+      { red: 0, green: 0, blue: 0 }
+    );
+
+    // Add the power of this game to the running sum
+    return sum + maxDiceCounts.red * maxDiceCounts.green * maxDiceCounts.blue;
+  }, 0);
+
+  return sumPowers;
 };
