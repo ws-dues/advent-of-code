2787fa38bbd2ebb121ef80ab2ea113d0e3d39225
Jacob Walker <ws-dues@users.noreply.github.com>
2023-12-06 21:03:33 -0600

    wip: day 3, 2023

--- 2023/Day3/src/Day3.ts
+++ 2023/Day3/src/Day3.ts
@@ -1,8 +1,24 @@
+import { join } from "path";
+import { syncReadFile } from "../../../utils";
+
 export const Day3Puzzle1 = () => {
-  console.log('Do something for puzzle 1, Day3');
+  // parse the input.txt file into a string
+  const engineText = syncReadFile(join(__dirname, "../input.txt"));
+  
+  // split the text file into rows
+  const engineRowsList = engineText.split('\n');
+
+  const enginePartNumbers = [];
+  for(let rowId = 0; rowId < engineRowsList.length; rowId++) {
+    const rowCharactersList = engineRowsList[rowId].replace(/\.+/g, " ").trim().split(' ');
+    console.log(rowCharactersList);
+    
+    for(let colId = 0; colId < rowCharactersList.length; colId++) {
+
+    }
+  }
 };
 
 export const Day3Puzzle2 = () => {
-  console.log('Do something for puzzle 2, Day3');
+  console.log("Do something for puzzle 2, Day3");
 };
-
--- 2023/Day3/src/index.ts
+++ 2023/Day3/src/index.ts
@@ -2,4 +2,3 @@ import { Day3Puzzle1, Day3Puzzle2 } from './Day3';
 
 console.log('Solution to Puzzle 1: ', Day3Puzzle1());
 console.log('Solution to Puzzle 2: ', Day3Puzzle2());
-
